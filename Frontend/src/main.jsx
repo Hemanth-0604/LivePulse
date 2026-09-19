a67a8ccbf3c4090a514acb0b874865ac41f48c5d
Hemanth Gopinathan <gopinathanhemanth@gmail.com>
2026-09-19 13:20:15 +0530

    Fix stray character in main.jsx; theme/header fixes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './styles/livepulse.css';0
+import './styles/livepulse.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
